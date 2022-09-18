66ed444956d4b9f83ee28fd8973863925b7ccdc8
bombaki6a <steliyan.dobrev66@gmail.com>
2022-09-18 15:02:29 +0300

    Edit user_profile

--- Resources/user_profile.ps1
+++ Resources/user_profile.ps1
@@ -1,12 +1,10 @@
 # Prompt
 Import-Module posh-git
-Import-Module oh-my-posh
-clear
 
 # Load prompt config
 function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
 $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'themes\bombaki6a-night-owl.omp.json'
-oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+oh-my-posh init pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
 # Alias
 Set-Alias ll ls
